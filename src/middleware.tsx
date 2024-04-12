43def19a3f1a4b0041dff254d9158e3f335d1636
KagnSeohyeon <kshyeon123@gmail.com>
2024-04-12 11:05:39 +0900

    :beer:test add middleware

---
+++ src/middleware.tsx
@@ -0,0 +1,29 @@
+import { NextMiddleware, NextRequest, NextResponse } from "next/server";
+
+const middleware: NextMiddleware = (request) => {
+  const res = NextResponse.next();
+
+  //other middleware operations
+  return res;
+};
+
+export default middleware;
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico|.swa).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
